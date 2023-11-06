6e7f07403ad9399289a45906e49c9dafcb6c7c27
Peter Phomthichack <67773109+peterphomthichack@users.noreply.github.com>
2023-11-06 15:47:38 -0600

    Update HW16.m

--- HW16.m
+++ HW16.m
@@ -1,44 +1,82 @@
 clear; clc;
 mylego = legoev3('usb');
-
-mycolorsensor = colorSensor(mylego);
-button = false;
-
 clearLCD(mylego);
+mycolorsensor = colorSensor(mylego);
 
-while(button == 0)
+% Variables storing the base frequency value for each color
+red_base_freg = 50;
+yellow_base_freg = 80;
+green_base_freg = 100;
+blue_base_freg = 120;
+brown_base_freg = 140;
+black_base_freg = 160;
+
+% timers
+delay = 0.3;
+tone_dur = 0.2;
+tone_vol = 10;
+
+
+start_button = false;
+% Checking the status of the module. If the "Up" button on the module is
+% pressed, the second while loop will execute the code below it.
+while(start_button == 0)
     if(readButton(mylego, 'up') == 1)
-        button = true;
-        pause(0.5);
-
+        start_button = true;
+        pause(delay);
     end
-    while(button == 1)
+    while(start_button == 1)
         clearLCD(mylego);
         color = readColor(mycolorsensor);
-        disp(color);
+        fprintf("Color: %s\n", color);
+        light_intensity = readLightIntensity(mycolorsensor); % reading the light intensity
+        fprintf("Light intensity: %d\n", light_intensity);
         if(color == "red")
-            writeLCD(mylego, 'red');
-            pause(1);
+            red = red_base_freg * light_intensity + 50; % store the value of base frequency multiply by light intensity plus 50
+            fprintf("Frequency: %d\n", red);
+            playTone(mylego, red , tone_dur, tone_vol);
+            writeLCD(mylego, 'Red');
+            pause(delay);
             clearLCD(mylego);
         elseif(color == "yellow")
-            writeLCD(mylego, 'yellow');
-            pause(1);
+            yellow = yellow_base_freg * light_intensity + 50;
+            fprintf("Frequency: %d\n", yellow);
+            playTone(mylego, yellow, tone_dur, tone_vol);
+            writeLCD(mylego, 'Yellow');
+            pause(delay);
             clearLCD(mylego);
         elseif(color == "green")
-            writeLCD(mylego, 'green');
-            pause(1);
+            green = green_base_freg * light_intensity + 50;
+            fprintf("Frequency: %d\n", green);
+            playTone(mylego, green, tone_dur, tone_vol);
+            writeLCD(mylego, 'Green');
+            pause(delay);
             clearLCD(mylego);
         elseif(color == "blue")
-            writeLCD(mylego, 'blue');
-            pause(1);
+            blue = blue_base_freg * light_intensity + 50;
+            fprintf("Frequency: %d\n", blue);
+            playTone(mylego, blue, tone_dur, tone_vol);
+            writeLCD(mylego, 'Blue');
+            pause(delay);
             clearLCD(mylego);
         elseif(color == "brown")
-            writeLCD(mylego, 'brown');
-            pause(1);
+            brown = brown_base_freg * light_intensity + 50;
+            fprintf("Frequency: %d\n", brown);
+            playTone(mylego, brown, tone_dur, tone_vol);
+            writeLCD(mylego, 'Brown');
+            pause(delay);
             clearLCD(mylego);
         elseif(color == "black")
-            writeLCD(mylego, 'black');
-            pause(1);
+            black = black_base_freg * light_intensity + 50;
+            fprintf("Frequency: %d\n", black);
+            playTone(mylego, black, tone_dur, tone_vol);
+            writeLCD(mylego, 'Black');
+            pause(delay);
+            clearLCD(mylego);
+        else
+            disp("Out of range");
+            writeLCD(mylego, 'Out of range');
+            pause(delay);
             clearLCD(mylego);
         end
     end
