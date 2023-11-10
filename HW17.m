5776ed644eb152092b779d06e54c1d8607726059
Peter Phomthichack <67773109+peterphomthichack@users.noreply.github.com>
2023-11-10 12:25:07 -0600

    Add files via upload

--- HW17.m
+++ HW17.m
@@ -13,22 +13,21 @@ while(start_button == 0)
     while(start_button)
         angle = abs(readRotationAngle(mygyrosensor));
         disp(angle);
-        if(angle == 0)
+        if(angle == 0) % North
             writeStatusLight(mylego, 'green', 'solid');
             playTone(mylego, 1500, 1, 10);
-            pause(.2);
-        elseif(angle == 180)
+            pause(0.2);
+        elseif(angle == 180) % South
             writeStatusLight(mylego, 'red', 'solid');
             playTone(mylego, 500, 1, 10);
-            pause(.2);
-        elseif(angle == 90)
+            pause(0.2);
+        elseif(angle == 90) % East and West
             writeStatusLight(mylego, 'orange', 'solid');
             playTone(mylego, 1000, 1, 10);
-            pause(.2);
+            pause(0.2);
         else
             writeStatusLight(mylego, 'orange', 'pulsing');
-            
+            pause(0.2);
         end
-    end
-    
+    end   
 end
