42b182daa578da30ccf5c84f97159d24fa232247
Peter Phomthichack <peterphomthichack@outlook.com>
2023-12-01 16:55:05 -0600

    fixing

--- FinalProject.m
+++ FinalProject.m
@@ -14,7 +14,7 @@ color_sensor = colorSensor(lego);
 
 % Motors initialization and settings
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-yellow_speed = 30;
+yellow_speed = 25;
 motor_speed = 40;
 right_motor = motor(lego,'C');
 left_motor = motor(lego,'B');
@@ -25,12 +25,13 @@ left_motor.Speed = motor_speed;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 first_stop = false;
+slowdown = false;
 checking_color = true;
 obstacles = false;
 
-angle_checking = true;
 resetRotationAngle(gyro_sensor);
 turning = true;
+
 while(start_program)
     if(readButton(lego, 'up') == 1)
         while(checking_color)
@@ -38,18 +39,19 @@ while(start_program)
             start(left_motor);
             color = readColor(color_sensor);
             checking_obstacles = readDistance(sonic_sensor);
-            if(color == "yellow")
+            if(color == "yellow" && slowdown == false)
                 writeLCD(lego, 'Approching a stop sign!');
                 writeStatusLight(lego, 'red', 'pulsing')
                 left_motor.Speed = yellow_speed;
                 right_motor.Speed = yellow_speed;
                 writeStatusLight(lego, 'off');
+                slowdown = true;
             end
             if(color == "red" && first_stop == false)
                 left_motor.Speed = motor_speed;
                 right_motor.Speed = motor_speed;
                 writeLCD(lego, 'Stopping at the stop sign!');
-                playTone(lego, 1000, 3, 10);
+                playTone(lego, 800, 3, 5);
                 writeStatusLight(lego, 'red', 'pulsing')
                 stop(right_motor, 1);
                 stop(left_motor, 1);
@@ -59,10 +61,10 @@ while(start_program)
                 first_stop = true;
             end
 
-            if(checking_obstacles <= 0.17 && turning == true)
+            if(checking_obstacles >= 0.17 && checking_obstacles <= 0.2 && turning == true)
                 disp(checking_obstacles);
-                stop(right_motor);
-                stop(left_motor);
+                stop(right_motor, 1);
+                stop(left_motor, 1);
                 pause(0.5);
                 left_motor.Speed = -motor_speed;
                 start(right_motor);
@@ -71,10 +73,12 @@ while(start_program)
                 while(turning)
                     angle = abs(readRotationAngle(gyro_sensor));
                     disp(angle);
-                    if(angle >= 85 && angle <= 95)
+                    if(angle >= 65 && angle <= 70) 
+                        stop(left_motor, 1);
+                        pause(0.2);
                         left_motor.Speed = motor_speed;
-                        stop(right_motor);
-                        stop(left_motor);
+                        start(right_motor);
+                        start(left_motor);
                         turning = false;
                     end
                 end
